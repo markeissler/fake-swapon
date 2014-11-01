c38fb8961113350212499b54b624b627f0ee21a2
Mark Eissler <mark@mixtur.com>
2014-10-31 17:47:42 -0700

    Fixed bug in OS detection. We were not setting a value for undetected OS.

--- fake-swapon.sh
+++ fake-swapon.sh
@@ -1165,28 +1165,27 @@ fi
 #
 echo "Analyzing system for fake-swap status..."
 
-runos=""
+runos="unknown"
 
 unamestr=$(${PATH_UNAME})
 case "${unamestr}" in
 "Linux" )
-  runos='linux'
+  runos="linux"
   ;;
 "FreeBSD" )
-  runos='freebsd'
+  runos="freebsd"
   ;;
 "Darwin" )
-  runos='osx'
+  runos="osx"
   ;;
 "SunOS" )
-  runos='solaris'
+  runos="solaris"
   ;;
 * )
-  runos=${platform}
   ;;
 esac
 
-if [ "${runos}" != 'linux' ]; then
+if [ "${runos}" != "linux" ]; then
   echo "ABORTING. Target OS doesn't appear to be Linux."
   echo
   exit 1
